594367e2f17cddb6053398977de67908c265c9aa
mcjcloud <brayden14cloud@gmail.com>
2017-12-31 00:47:10 -0600

    Created Eclipse project

---
+++ pdfinjector/src/main/java/PDFInjector/pdfinjector/App.java
@@ -0,0 +1,14 @@
+package PDFInjector.pdfinjector;
+
+/**
+ * App entry
+ * check parameters passed to application, if everything is good, process params
+ *
+ * @author mcjcloud
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
